95f3922c982f176705303ee43edfc68b601b537c
Raphael Freitas <dev.rapha@gmail.com>
2018-01-28 21:26:11 -0200

    estudos com redux e javascript

--- documentation-redux/app.js
+++ documentation-redux/app.js
@@ -3,21 +3,14 @@ const { addTodo, toggleTodo, setVisibilityFilter, VisibilityFilters } = require(
 const { todoApp } = require('./reducers.js');
 
 let store = createStore(todoApp);
-console.log('**************')
-console.log('')
-
-console.log(store.getState())
 
 store.dispatch(addTodo('test true'))
 store.dispatch(addTodo('test false'))
-console.log(store.getState())
+console.log(store.getState());
 
 store.dispatch(toggleTodo(0))
-console.log(store.getState())
+console.log(store.getState());
 
 store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED))
-console.log(store.getState())
 
-console.log('')
-console.log('**************')
 
